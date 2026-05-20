aaa3b87c0e11dc7bb9d5930234301430b0a7a923
Mansi Gaurav <mansigaurav407@gmail.com>
2026-05-21 00:36:25 +0530

    Refactor Sidebar and add logout functionality
    Refactor Sidebar component by removing unused menu items and updating the title. Added logout functionality.

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,14 +1,11 @@
 import { useNavigate } from "react-router-dom";
 import "../css/Sidebar.css";
-
 import { useState } from "react";
 
 import {
   FaThLarge,
-  FaHeart,
   FaHamburger,
   FaCommentDots,
-  FaMoneyBill,
   FaCog,
   FaSignOutAlt
 } from "react-icons/fa";
@@ -18,95 +15,61 @@ function Sidebar() {
   const [activeMenu, setActiveMenu] = useState("Dashboard");
   const navigate = useNavigate();
 
+  //  LOGOUT FUNCTION
+  const handleLogout = () => {
+    localStorage.clear(); // clear user data
+    navigate("/"); // go to Auth page
+  };
 
   const menus = [
-
-    {
-      name: "Dashboard",
-      icon: <FaThLarge />,
-    },
-
-    {
-      name: "Favorites",
-      icon: <FaHeart />,
-    },
-
-    {
-      name: "Orders",
-      icon: <FaHamburger />,
-    },
-
-    {
-      name: "Messages",
-      icon: <FaCommentDots />,
-    },
-
-    {
-      name: "Bills",
-      icon: <FaMoneyBill />,
-    },
-
-    {
-      name: "Settings",
-      icon: <FaCog />,
-    },
-
-    {
-      name: "Logout",
-      icon: <FaSignOutAlt />,
-    },
-
+    { name: "Dashboard", icon: <FaThLarge /> },
+    { name: "Orders", icon: <FaHamburger /> },
+    { name: "Messages", icon: <FaCommentDots /> },
+    { name: "Settings", icon: <FaCog /> },
+    { name: "Logout", icon: <FaSignOutAlt /> },
   ];
 
-
-
   return (
-
     <div className="sidebar">
 
       {/* LOGO */}
-      <h1 className="logo">
-        Cafeteria
+      <h1 className="sidebar-title">
+        Chilli's <br />
+        Food <br />
+        Point
       </h1>
-
-
-
 
       {/* MENU */}
       <ul className="menu">
-
-        {
-
-          menus.map((item, index) => (
-
-            <li
+        {menus.map((item, index) => (
+          <li
             key={index}
-            className={
-              activeMenu === item.name
-                ? "active"
-                : ""
-            }
-          
+            className={activeMenu === item.name ? "active" : ""}
             onClick={() => {
               setActiveMenu(item.name);
-          
+
+              //  ROUTING LOGIC
               if (item.name === "Orders") {
                 navigate("/orders");
+              }
+
+              if (item.name === "Settings") {
+                navigate("/settings");
+              }
+
+              //  LOGOUT CLICK
+              if (item.name === "Logout") {
+                handleLogout();
               }
             }}
           >
             {item.icon}
             {item.name}
-          </li> 
-
-          ))
-
-        }
-
+          </li>
+        ))}
       </ul>
 
     </div>
-
   );
 }
 
